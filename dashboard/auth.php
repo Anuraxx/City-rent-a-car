16a294a4aa3c1d19cdcfee0b28241b02848f994a
Anurag <omavihso@gmail.com>
2021-02-11 19:21:47 +0530

    make server url dynamic

--- dashboard/auth.php
+++ dashboard/auth.php
@@ -4,9 +4,18 @@
     $username=$_POST["email"];
     $passcode=$_POST["pass"];
 
-   
-    //$server_url= "https://fortunate-versed-clematis.glitch.me/auth?username=$username&passcode=$passcode";
-    $server_url="http://localhost:3000/auth?username=$username&passcode=$passcode";
+   //$server_url= "http://localhost:3000/auth?username=$username&passcode=$passcode";
+    
+    $server = new \stdClass();
+    if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')  
+        $server->protocol = "https";
+    else $server->protocol = "http";
+    $server->host= $_SERVER['HTTP_HOST'];
+    $server->port= $server->host=='localhost'?':3000':'';
+    
+    $server_url="$server->protocol://$server->host$server->port/auth?username=$username&passcode=$passcode";
+
+    
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
     curl_setopt($ch, CURLOPT_URL, $server_url); 
@@ -28,5 +37,5 @@
             header("Location: ./login.php");
         }
 
-    } 
+    }  
 ?>
